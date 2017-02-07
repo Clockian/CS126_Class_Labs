dd320587abeec71b0677dd91a4d3da9bef945f79
Jasque Saydyk <jrsaydyk@gmail.com>
2017-02-07 16:14:58 -0700

    Refactor Repo
    Refactor repo to be used for entire class

---
+++ Lab 01/madlib.py
@@ -0,0 +1,46 @@
+name = input("Input a Name: ")
+vegetable = input("Input a Vegetable: ")
+candy = input("Input a Candy: ")
+capital_name = input("Input a Capital Name: ")
+spice = input("Input a Spice: ")
+job_title = input("Input a Job Title: ")
+adjective = input("Input an Adjective: ")
+gourd = input("Input a Gourd: ")
+celestial_body = input("Input a Celestial Body: ")
+emotion = input("Input an Emotion: ")
+liquid = input("Input a Liquid: ")
+store = input("Input a Store: ")
+
+print("\nOnce upon a time, " + name + " was magically transported" +
+      "\nto a magical kingdom on the brink of collapse. The mighty\n" +
+      vegetable + " have clashed with the " + candy + " Empire and" +
+      "\ndriven the Empire into their walls of their most mighty city,\n" +
+      capital_name + ", which has definitely fended of countless invasions" +
+      "\nin years past. But the " + vegetable + " were like no past invading" +
+      "\nforce, bringing with them the power of " + spice + ", they blasted" +
+      "\nat the mighty walls, reducing them to rubble, yet the " + candy +
+      "\npeople fought back to resist the irresistible tide of the\n" +
+      vegetable + "washing over them.\n")
+
+print(name + " was taken to the last " + job_title + "of the " + candy +
+      "\npeople, who pleaded with " + name + " to fight alongside him" +
+      "\nand give his Empire a fruitless chance to see another" +
+      "\nday. This man agreed, and went out to battle to give victory to" +
+      "\nthe" + candy + " people. But after hours of " + adjective +
+      "\nfighting, " + name + " finally saw the commander of the\n" +
+      vegetable + " forces, an imposing " + gourd + " with " + spice +
+      "\nbaked into his pores, thundering to his forces that \n" +
+      celestial_body + " has promised them victory this day.\n")
+
+print("The scent wafting off of the commander sparked" +
+      "\nthe " + emotion + " inside of " + name + ", who went" +
+      "\nmad and started eating all of the nearby soldiers.\n" +
+      emotion + ", never ceasing, gnawed at the insides of\n" +
+      name + ", and the more " + name + " ate, the more it" +
+      "\ngrew. The nearby soldiers panicked at the sight of the" +
+      "\nman, attempting to flee in vain as " + name + " ate"
+      "\nmore and more of their kin. In short order, the battlefield" +
+      "\nis silent, as all the competing food now laid in rest," +
+      "\nslowly dissolving all away in the acidic " + liquid +
+      "\nof the stomach of " + name + ", who now slept in peace" +
+      "\ninside of " + store + ".")
