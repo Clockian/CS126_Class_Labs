388a09718dab0ede402f6826853fe03647050cd8
Jasque Saydyk <jrsaydyk@gmail.com>
2017-02-21 20:31:05 -0700

    Pep8 compliant, fixed small issues
    fixed issue regarding weights incorrectly set, fully pep8 compliant

--- Lab 04/GradeCalculator.py
+++ Lab 04/GradeCalculator.py
@@ -2,7 +2,7 @@
 # Lab 04 - Grade Calculator
 # Section 2, Feb. 15, 2017
 # Description - This is a simple grade calculator that
-# uses a set of methods
+# using a set predefined set of methods
 
 # Data
 homeworkScore = [39, 40, 29, 40, 0, 5]
@@ -11,11 +11,14 @@ homeworkWeight = 0.2
 
 quizScore = [10, 10, 9, 2, 10, 10, 10]
 quizMax = [10, 10, 10, 10, 10, 10, 10]
-quizWeight = 0.6
+quizWeight = 0.2
 
 testScore = [293, 284, 300]
 testMax = [300, 300, 300]
-testWeight = 0.2
+testWeight = 0.6
+
+percent_list = []
+
 
 def percentage_per_category(score_list, max_list):
     """
@@ -25,25 +28,27 @@ def percentage_per_category(score_list, max_list):
         max_list - A list of maximum possible points. Should the be same length
         as score_list
     Return:
-        returns a floating point number representing the percentage of points earned
-        in the given category.
+        returns a floating point number representing the percentage of points
+        earned in the given category.
     """
     numerator = sum(score_list)
     denominator = sum(max_list)
     return ((numerator/denominator) * 100)
 
-def letter_grade (percent):
+
+def letter_grade(percent):
     """
     Return the letter grade of the given percentage.
     Parameters:
-        percent - A floating point number from 0 to 1, representing your percentage score.
+        percent - A floating point number from 0 to 1, representing your
+        percentage score.
     Returns:
-        The string "A", "B", "C", "D", "F" depending on the percentage, at the 90%, 80%,
-        70%, 60% thresholds.
+        The string "A", "B", "C", "D", "F" depending on the percentage, at
+        the 90%, 80%, 70%, 60% thresholds.
     """
     if percent >= 90:
         return "A"
-    
+
     elif percent >= 80:
         return "B"
 
@@ -56,57 +61,43 @@ def letter_grade (percent):
     else:
         return "F"
 
-# Todo: Double check, may not calculate correctly despite lab
-def weighted_score (percentage , weight) :
+
+def weighted_score(percentage, weight):
     """
-    Calculates the weighted contribution of a category to the overall course grade.
+    Calculates the weighted contribution of a category to the overall
+    course grade.
     Parameters:
-        percentage - The percentage of possible points earned in this category.
-        weight - A number between 0 and 1 representing the weight of this category.
+        percentage - The percentage of possible points earned in this
+        category.
+        weight - A number between 0 and 1 representing the weight of
+        this category.
     Return:
-        returns a floating point number representing the amount the given category
-        contributes to the overall grade.
+        returns a floating point number representing the amount the
+        given category contributes to the overall grade.
     """
     return (percentage * weight)
 
 # Todo: Extra Credit Methods
-""""
-def to_percent(score_list , max_list):
+# Not going to do extra credit methods that deny obvious or functional
+# solution to problem
 
-    for i in range(len(score_list)):
-
-        percent[i] =( score_list[i]/max_list[i] )
-
-    return percent
-# re-check this 
-def median ( score_list , max_list):
-
-    for i in range(len(score_list)):
-
-        percent[i] =( score_list[i]/max_list[i] )
-
-        
-    percent = sum(percent)/len(percent)
-    
-    return percent
-"""
-
-# Print the percentages and letter grade for each category, and the final weighted score and letter grade
-# Round the averages to the nearest percent
+# Print the percentages and letter grade for each category, and the final
+# weighted score and letter grade. Round the averages to the nearest percent
 homeworkPercent = percentage_per_category(homeworkScore, homeworkMax)
 homeworkTotal = weighted_score(homeworkPercent, homeworkWeight)
-print("Homework: " + str(int(homeworkPercent)) + "%, Grade: " + letter_grade(homeworkPercent) +
-      ", Final Weighted Score: " + str(homeworkTotal) + ".")
+print("Homework Grade: " + str(int(homeworkPercent)) + "%, Grade: " +
+      letter_grade(homeworkPercent))
 
 quizPercent = percentage_per_category(quizScore, quizMax)
 quizTotal = weighted_score(quizPercent, quizWeight)
-print("Quiz: " + str(int(quizPercent)) + "%, Grade: " + letter_grade(quizPercent) +
-      ", Final Weighted Score: " + str(quizTotal) + ".")
+print("    Quiz Grade: " + str(int(quizPercent)) + "%, Grade: " +
+      letter_grade(quizPercent))
 
 testPercent = percentage_per_category(testScore, testMax)
 testTotal = weighted_score(testPercent, testWeight)
-print("Test: " + str(int(testPercent)) + "%, Grade: " + letter_grade(testPercent) +
-      ", Final Weighted Score: " + str(testTotal) + ".")
+print("    Test Grade: " + str(int(testPercent)) + "%, Grade: " +
+      letter_grade(testPercent))
 
 finalScore = homeworkTotal + quizTotal + testTotal
-print("Final Score: " + str(int(finalScore)) + "%, Grade: " + letter_grade(finalScore))
+print("   Final Score: " + str(int(finalScore)) + "%, Grade: " +
+      letter_grade(finalScore))
